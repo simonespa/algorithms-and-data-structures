23e0e57f14f8b8a1f2514df3536d8eab11b3b2eb
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2021-08-28 13:52:59 +0100

    push changes

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -31,7 +31,7 @@ export default {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -117,9 +117,10 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  "roots": [
+    "<rootDir>/test",
+    "<rootDir>/src"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -146,10 +147,10 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -172,9 +173,9 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
--- test/collections/NumericHeap.test.ts
+++ test/collections/NumericHeap.test.ts
@@ -1,1 +1,10 @@
-import NumericHeap from 'src/collections/NumericHeap';
+import NumericHeap, { HeapType } from '../../src/collections/NumericHeap';
+
+describe('NumericHeap', () => {
+  test('basic', () => {
+    const input = [2, 4, 7, 1, 5, 3];
+    const expected = [7, 5, 3, 1, 4, 2];
+    const heap = new NumericHeap(input, HeapType.MAX);
+    expect(heap.toArray()).toEqual(expected);
+  });
+});
