2bfd86160ac8b86064c263a8ed1f12b3e2e7c03e
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2022-04-01 15:26:12 +0100

    Removed index file

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import BinaryNode from "./collections/BinaryNode";
-
-const n = new BinaryNode(3);
-console.log(n.getValue());
-console.log(n.isLeafNode());
-console.log(n.isRootNode());
-console.log(n.isVisited());
