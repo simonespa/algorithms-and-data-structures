b3e182e25d235b414799bfeb163914ed35c1ff9e
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2023-10-17 11:01:20 +0100

    Enable Dependabot (#33)

---
+++ src/exercises/majority-elements/index.ts
@@ -0,0 +1,18 @@
+export function majority(nums: number[]): number {
+  const n = nums.length
+  const map = new Map();
+
+  for (const num of nums) {
+      let counter = map.get(num)
+
+      if (counter === undefined) {
+          counter = 1
+      } else {
+          counter++
+      }
+
+      if (counter >= n / 2) return num;
+
+      map.set(num, counter)
+  }
+}
