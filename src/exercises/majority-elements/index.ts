b062fbdaf7a5851cb449fec1a2b3dc01d8e36f33
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2023-10-27 16:53:58 +0100

    Update

--- src/exercises/majority-elements/index.ts
+++ src/exercises/majority-elements/index.ts
@@ -1,18 +1,18 @@
 export function majority(nums: number[]): number {
-  const n = nums.length
+  const n = nums.length;
   const map = new Map();
 
   for (const num of nums) {
-      let counter = map.get(num)
+    let counter = map.get(num);
 
-      if (counter === undefined) {
-          counter = 1
-      } else {
-          counter++
-      }
+    if (counter === undefined) {
+      counter = 1;
+    } else {
+      counter++;
+    }
 
-      if (counter >= n / 2) return num;
+    if (counter >= n / 2) return num;
 
-      map.set(num, counter)
+    map.set(num, counter);
   }
 }
