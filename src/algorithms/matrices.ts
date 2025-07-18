6fbccd0217d2917b93c78cca5bb4776b55e527de
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2025-07-18 17:27:00 +0100

    Update

--- src/algorithms/matrices.ts
+++ src/algorithms/matrices.ts
@@ -5,7 +5,6 @@
 
 export function sum(matrix: number[][]): number {
   let sum = 0;
-  console.log(matrix);
 
   for (let i = 0; i < matrix.length; i += 1) {
     for (let j = 0; j < matrix[i].length; j += 1) {
